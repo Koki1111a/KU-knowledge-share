ad890c503b4e446c18e7cb92034c7ae557ebf6af
asakusabashi0593 <0wy5fm2k388534z@ezweb.ne.jp>
2025-06-05 20:52:38 +0900

    add about-section

--- Portfolio/js/mask-canvas.js
+++ Portfolio/js/mask-canvas.js
@@ -1,17 +1,72 @@
-const images = [
-  'https://kyoto-sakura.net/img/ujigawa-haryu15.jpg',
-  // '../images/hero-sumemr.jpg',
-  './images/hero-autumn.jpg',
-  './images/hero-winter.jpg'
-];
+document.addEventListener("DOMContentLoaded", function () {
+  // ローディング開始時に body に 'loading' クラスを付けて固定
+  document.body.classList.add('loading');
 
-let current = 0;
-const heroSection = document.getElementById('hero-section');
+  Pace.start({
+    ajax: {
+      trackMethods: ["GET", "POST"],
+    },
+    startOnPageLoad: true,
+    restartOnPushState: true,
+    minTime: 3000
+  });
 
-// 初期表示
-heroSection.style.backgroundImage = `url(${images[current]})`;
+  // ローディング完了後の処理
+  Pace.on('done', function () {
+    // スクロールロック解除
+    
 
-setInterval(() => {
-  current = (current + 1) % images.length;
-  heroSection.style.backgroundImage = `url(${images[current]})`;
-}, 5000); // 5000ms = 5秒ごとに切り替え
+    // メインコンテンツをフェードイン
+    const mainContent = document.querySelector(".main-content");
+    if (mainContent) {
+      mainContent.style.opacity = "1";
+      document.body.classList.remove('loading');
+    }
+
+    // 背景画像切り替えロジック
+    const images = [
+      './images/hero-autumn.jpg',
+      'https://kyoto-sakura.net/img/ujigawa-haryu15.jpg',
+      './images/hero-winter.jpg'
+    ];
+    let current = 0;
+    const heroSection = document.getElementById('hero-section');
+    if (heroSection) {
+      heroSection.style.backgroundImage = `url(${images[current]})`;
+
+      setInterval(() => {
+        current = (current + 1) % images.length;
+        heroSection.style.backgroundImage = `url(${images[current]})`;
+      }, 4000);
+    }
+  });
+});
+
+
+/*About section*/
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutSection = document.querySelector('.about-section');
+
+  window.addEventListener('scroll', () => {
+    const rect = aboutSection.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    if (rect.top <= windowHeight * 0.8) {
+      aboutSection.classList.add('visible');
+    }
+  });
+});
+
+/*About section underline */
+document.addEventListener("DOMContentLoaded", function () {
+  const target = document.querySelector('.underline');
+
+  window.addEventListener('scroll', () => {
+    const rect = target.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    if (rect.top <= windowHeight * 0.8) {
+      target.classList.add('visible');
+    }
+  });
+});
